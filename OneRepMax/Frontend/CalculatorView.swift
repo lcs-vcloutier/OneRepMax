d638bb44f016a3475f037ca0640b9d57372cc72e
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 16:52:29 -0400

    Successfully displaying a list of predictions of maxes at different reps.

--- OneRepMax/Frontend/CalculatorView.swift
+++ OneRepMax/Frontend/CalculatorView.swift
@@ -10,20 +10,32 @@ import SwiftUI
 struct CalculatorView: View {
     @State private var oneRepMax = ""
     @State private var exercise = ExercisePossibility.squat
+    @State private var results = [0]
+    @State private var tracker = 0
+    
     var body: some View {
         Form {
-            Section(header: Text("INPUT WEIGHT")) {
+            Section(header: Text("INPUTS")) {
                 TextField("0", text: $oneRepMax)
                     .keyboardType(.decimalPad)
                     .font(.title)
-            }
-            Section(header: Text("RESULTS")) {
                 Picker("EXERCISE", selection: $exercise) {
                     Text(ExercisePossibility.squat.rawValue).tag(ExercisePossibility.squat)
                     Text(ExercisePossibility.bench.rawValue).tag(ExercisePossibility.bench)
                     Text(ExercisePossibility.deadlift.rawValue).tag(ExercisePossibility.deadlift)
                 }
                 .pickerStyle(.segmented)
+                Button {
+                    results = calculate(oneRepMax: Int(oneRepMax)!, exercise: exercise)
+                } label: {
+                    Text("Submit")
+                }
+                .disabled(oneRepMax.isEmpty)
+            }
+            Section(header: Text("RESULTS")) {
+                List(results.indices, id: \.self) { index in
+                    Text("\(index + 1)RM = \(self.results[index])LBS")
+                }
             }
         }
         .navigationTitle("Calculator")
@@ -41,5 +53,6 @@ func calculate(oneRepMax: Int, exercise: ExercisePossibility) -> [Int] {
             calculation.append(oneRepMax+index)
         }
     }
+    calculation.removeFirst()
     return calculation
 }
