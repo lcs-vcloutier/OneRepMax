b419ea439dd8e33c8405bc6e13bcd06810746cc3
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 15:57:17 -0400

    Laid out initial UI, results will go below picker in the same section.

--- OneRepMax/Frontend/CalculatorView.swift
+++ OneRepMax/Frontend/CalculatorView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct CalculatorView: View {
+    @State private var oneRepMax = ""
+    @State private var exercise = ExercisePossibility.squat
     var body: some View {
         Form {
-            
+            Section(header: Text("INPUT WEIGHT")) {
+                TextField("0", text: $oneRepMax)
+                    .keyboardType(.decimalPad)
+                    .font(.title)
+            }
+            Section(header: Text("RESULTS")) {
+                Picker("EXERCISE", selection: $exercise) {
+                    Text(ExercisePossibility.squat.rawValue).tag(ExercisePossibility.squat)
+                    Text(ExercisePossibility.bench.rawValue).tag(ExercisePossibility.bench)
+                    Text(ExercisePossibility.deadlift.rawValue).tag(ExercisePossibility.deadlift)
+                }
+                .pickerStyle(.segmented)
+            }
         }
         .navigationTitle("Calculator")
     }
