72d1242e1c70ff8d96966a525f41ccc90c68a704
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 14:19:31 -0400

    Laid out form view.

--- OneRepMax/Frontend/AddEntryView.swift
+++ OneRepMax/Frontend/AddEntryView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct AddEntryView: View {
+    // CALL IN VM HERE
     @Binding var showing: Bool
+    @State private var date = Date()
+    @State private var exercise = ExercisePossibility.squat
+    @State private var weight = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Weight")) {
+                    TextField("225", text: $weight)
+                        .keyboardType(.decimalPad)
+                        .font(.title)
+                }
+                Section(header: Text("Exercise")) {
+                    Picker("EXERCISE", selection: $exercise) {
+                        Text(ExercisePossibility.squat.rawValue).tag(ExercisePossibility.squat)
+                        Text(ExercisePossibility.bench.rawValue).tag(ExercisePossibility.bench)
+                        Text(ExercisePossibility.deadlift.rawValue).tag(ExercisePossibility.deadlift)
+                    }
+                    .pickerStyle(.segmented)
+                }
+                DatePicker("", selection: $date, displayedComponents: .date)
+                    .datePickerStyle(.graphical)
+            }
+            .navigationTitle(exercise.rawValue)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Save") {
+                        // SAVE TASK HERE
+                        showing = false
+                    }
+                    .disabled(weight.isEmpty)
+                }
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        showing = false
+                    }
+                }
+            }
+        }
+        .interactiveDismissDisabled()
     }
 }
 
