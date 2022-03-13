9ee6da00b8c7f7b2b2c2b362032a6fac716aa23b
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 13:58:54 -0400

    Created basic view where user will add entries later.

---
+++ OneRepMax/Frontend/AddEntryView.swift
@@ -0,0 +1,16 @@
+//
+//  AddEntryView.swift
+//  OneRepMax
+//
+//  Created by Vincent Cloutier on 2022-03-13.
+//
+
+import SwiftUI
+
+struct AddEntryView: View {
+    @Binding var showing: Bool
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
