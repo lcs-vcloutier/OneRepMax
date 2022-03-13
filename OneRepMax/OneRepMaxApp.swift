b94fa1f106c867a8a70e6ddc519f46061beae81d
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 14:57:20 -0400

    Allows app to persist tasks when backgrounded.

--- OneRepMax/OneRepMaxApp.swift
+++ OneRepMax/OneRepMaxApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct OneRepMaxApp: App {
+    @Environment(\.scenePhase) var scenePhase
     @StateObject private var store = EntryStore(entries: testData)
     var body: some Scene {
         WindowGroup {
@@ -25,6 +26,16 @@ struct OneRepMaxApp: App {
                 .tabItem {
                     Label("Calculator", systemImage: "apps.iphone.badge.plus")
                 }
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .inactive {
+                print("Inactive")
+            } else if newPhase == .active {
+                print("Active")
+            } else if newPhase == .background {
+                print("Backgrounded")
+                store.persistEntries()
             }
         }
     }
