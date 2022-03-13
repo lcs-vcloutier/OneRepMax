d48b21b1f9bcbc3e3cf7558f5d7be39152cb5926
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 14:55:51 -0400

    Created VM that allows to save to a single entry to the entries array (used to update list view), it also persists the whole array.

--- OneRepMax/Backend/ViewModel.swift
+++ OneRepMax/Backend/ViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+}
+
+class EntryStore: ObservableObject {
+    
+    // Stored propeties
+    @Published var entries: [Entry]
+    
+    // Initializers
+    init(entries: [Entry] = []) {
+        let filename = getDocumentsDirectory().appendingPathComponent("savedEntries")
+        do {
+            let data = try Data(contentsOf: filename)
+            self.entries = try JSONDecoder().decode([Entry].self, from: data)
+        } catch {
+            print(error.localizedDescription)
+            self.entries = entries
+        }
+    }
+    
+    // Save entry to entries array.
+    func saveEntry(date: Date, exercise: ExercisePossibility, weight: Int) {
+        entries.append(Entry(date: date, exercise: exercise, weight: weight))
+    }
+    
+    // Persist entire entries array.
+    func persistEntries() {
+        let filename = getDocumentsDirectory().appendingPathComponent("savedEntries")
+        do {
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            let data = try encoder.encode(self.entries)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
