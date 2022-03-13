0222d3df1748b4a2676d6d22f2ba1ff7aa0e269b
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 13:59:16 -0400

    Laid out ListView and connected it to the add entry view.

--- OneRepMax/Frontend/ListView.swift
+++ OneRepMax/Frontend/ListView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct ListView: View {
+    @State private var showingAddEntry = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(testData) { testData in
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(testData.exercise.rawValue)
+                    Spacer()
+                    Text("\(testData.weight)LBS")
+                }
+                .font(.largeTitle)
+                Text(testData.date, style: .date)
+                    .font(.caption)
+            }
+        }
+        .navigationTitle("One Rep Max")
+        .toolbar {
+            ToolbarItem {
+                Button {
+                    showingAddEntry = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddEntry) {
+            AddEntryView(showing: $showingAddEntry)
+        }
     }
 }
