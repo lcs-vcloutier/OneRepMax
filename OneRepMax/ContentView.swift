487ab783f2a1684817867c0cad6070e04519f7fd
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 12:42:56 -0400

    Initial Commit

---
+++ OneRepMax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneRepMax
+//
+//  Created by Vincent Cloutier on 2022-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneRepMax/OneRepMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneRepMaxApp.swift
+//  OneRepMax
+//
+//  Created by Vincent Cloutier on 2022-03-13.
+//
+
+import SwiftUI
+
+@main
+struct OneRepMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
