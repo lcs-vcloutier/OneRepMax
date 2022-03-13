612743362f027775a240d92990ab87c9ee53928d
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-03-13 13:48:28 -0400

    Added an enum for potential exercises, a struct called that keeps track of a single entry as well as some test data.

--- OneRepMax/Backend/Model.swift
+++ OneRepMax/Backend/Model.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+enum ExercisePossibility: String, Codable {
+    case squat = "Squat"
+    case bench = "Bench"
+    case deadlift = "Deadlift"
+}
+
+struct Entry: Identifiable {
+    var id = UUID()
+    var date: Date
+    var exercise: ExercisePossibility
+    var weight: Int
+}
+
+let testData = [
+    Entry(date: Date(), exercise: ExercisePossibility.bench, weight: 225),
+    Entry(date: Date(), exercise: ExercisePossibility.squat, weight: 315),
+    Entry(date: Date(), exercise: ExercisePossibility.deadlift, weight: 405)
+]
